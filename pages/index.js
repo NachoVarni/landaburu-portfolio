0e0ea527471b2ab549ea095c448c41fc70694f21
Nacho Varni <nachovarni6@gmail.com>
2021-08-31 15:24:11 -0300

    improving performance

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Adventadges from "../components/Advantadges";
-import Education from "../components/Education";
-import Projects from "../components/Projects";
-import FindMe from "../components/FindMe";
-import Footer from "../components/Footer";
+const Adventadges = dynamic(() => import("../components/Advantadges"));
+const Education = dynamic(() => import("../components/Education"));
+const Projects = dynamic(() => import("../components/Projects"));
+const FindMe = dynamic(() => import("../components/FindMe"));
+const Footer = dynamic(() => import("../components/Footer"));
+import useInView from "react-cool-inview";
 
 export default function Home() {
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(),
+  });
+
   return (
     <div className="bg-grayBg ">
       <Head>
@@ -19,14 +25,14 @@ export default function Home() {
         />
         <meta property="og:title" content="My personal page" />
       </Head>
-      <div className="w-11/12 md:w-5/6 m-auto font-poppins">
+      <div className="w-11/12 md:w-5/6 m-auto font-poppins" ref={observe}>
         <Navbar />
         <Hero />
-        <Adventadges />
-        <Education />
-        <Projects />
-        <FindMe />
-        <Footer />
+        {inView && <Adventadges />}
+        {inView && <Education />}
+        {inView && <Projects />}
+        {inView && <FindMe />}
+        {inView && <Footer />}
       </div>
     </div>
   );
